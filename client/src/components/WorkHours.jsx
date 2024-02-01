56efc1e7be852d8b32db9d63db37f523dc019851
Nilsson <marcus.nilsson@cgi.com>
2024-02-01 11:16:19 +0100

    fixing so that workingHourse is back to normal

--- client/src/components/WorkHours.jsx
+++ client/src/components/WorkHours.jsx
@@ -1,8 +1,7 @@
 import { styles } from "../styles";
-import { format } from "date-fns"; // Import date-fns for formatting
 import { meetings } from "../helpers/mockData";
 
-export const generateHourlyStructure = (currentDate) => {
+export const generateHourlyStructure = () => {
   const hours = Array.from({ length: 10 }, (_, i) => i + 8);
 
   return (
@@ -13,20 +12,7 @@ export const generateHourlyStructure = (currentDate) => {
           className="justify-start items-center gap-[5.63px] inline-flex"
         >
           <div className={`${styles.workHoursStyle} `}>{hour}</div>
-          <div className="w-[140.15px] h-[0px] border border-thin border-neutral-500 ">
-            {/* Render meetings for the current day and time */}
-            {meetings
-              .filter(
-                (meeting) =>
-                  meeting.date === format(currentDate, "d MMM") &&
-                  parseInt(meeting.fromTime.split(":")[0]) === hour
-              )
-              .map((meeting) => (
-                <div key={meeting.id}>
-                  {meeting.title} ({meeting.fromTime} - {meeting.toTime})
-                </div>
-              ))}
-          </div>
+          <div className="w-[140.15px] h-[0px] border border-thin border-neutral-500 "></div>
         </div>
       ))}
     </div>
