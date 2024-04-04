50dd27a10a03ac7e596f1b3b8765c8b68683f7fc
BoMar-Dev <marcusnilsson2010@gmail.com>
2024-04-04 11:50:55 +0200

    upload img is rendering on gallery page

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-
-import imgTest from "./../../public/img/unsplashed3.jpg";
+import { useState, useEffect } from "react";
 
 // Icons
 import { IoIosPlay } from "react-icons/io";
@@ -9,30 +7,31 @@ import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageNames, setImageNames] = useState([]);
   const [file, setFile] = useState(null);
 
-  const imgs = [
-    {
-      url: imgTest,
-    },
-    {
-      url: "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=1875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=1874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1519865885898-a54a6f2c7eea?q=80&w=1358&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
+  useEffect(() => {
+    fetch("http://localhost:3001/api/galleri/img")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setImageNames(data);
+      })
+      .catch((error) => console.error("Error fetching image names:", error));
+  }, []);
 
   const prevImg = () => {
-    const isFirsImg = currentIndex === 0;
-    const newIndex = isFirsImg ? imgs.length - 1 : currentIndex - 1;
+    const isFirstImg = currentIndex === 0;
+    const newIndex = isFirstImg ? imageNames.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+
   const nextImg = () => {
-    const isLastImg = currentIndex === imgs.length - 1;
+    const isLastImg = currentIndex === imageNames.length - 1;
     const newIndex = isLastImg ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
@@ -62,14 +61,14 @@ const Slider = () => {
   };
 
   const handleUploadAndReload = () => {
-    handleUpload(); // Call handleUpload function
-    setFile(null); // Clear the selected file state
-    window.location.reload(); // Reload the page
+    handleUpload();
+    setFile(null);
+    window.location.reload();
   };
 
   return (
     <div className="w-[1800px] h-[1080px]   relative bg-black">
-      <div style={{ backgroundImage: `url(${imgs[currentIndex].url})` }} className="w-full bg-cover h-full bg-center duration-500">
+      <div style={{ backgroundImage: `url(/img/${imageNames[currentIndex]})` }} className="w-full bg-cover h-full bg-center duration-500">
         <div className="p-5">
           <div>
             <label htmlFor="file" className="sr-only">
@@ -95,16 +94,13 @@ const Slider = () => {
           )}
         </div>
         <footer className="flex h-10 bg-gray-500 justify-center  absolute bottom-0 w-full">
-          {/* Right arrow  */}
           <div className="mr-10 mt-1">
             <MdOutlineKeyboardArrowLeft onClick={prevImg} cursor="pointer" size="2rem" />
           </div>
-          {/* // Play */}
           <div className="mt-[8px] ">
             <IoIosPlay size="1.5rem" />
           </div>
           <div className="ml-10 mt-1">
-            {/* Left arrow  */}
             <RiArrowRightSLine onClick={nextImg} cursor="pointer" size="2rem" />
           </div>
         </footer>
